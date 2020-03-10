d299dd2aad4f09a4ac44a546b3266ba474bc0f2c
Dries <s110169@ap.be>
2020-03-10 15:02:00 +0100

    weird add commit

--- thermo-sauna/app/build/generated/not_namespaced_r_class_sources/debug/r/de/hdodenhof/circleimageview/R.java
+++ thermo-sauna/app/build/generated/not_namespaced_r_class_sources/debug/r/de/hdodenhof/circleimageview/R.java
@@ -12,15 +12,15 @@ public final class R {
     public static final class attr {
         private attr() {}
 
-        public static final int civ_border_color = 0x7f030049;
-        public static final int civ_border_overlay = 0x7f03004a;
-        public static final int civ_border_width = 0x7f03004b;
-        public static final int civ_circle_background_color = 0x7f03004c;
+        public static final int civ_border_color = 0x7f03004a;
+        public static final int civ_border_overlay = 0x7f03004b;
+        public static final int civ_border_width = 0x7f03004c;
+        public static final int civ_circle_background_color = 0x7f03004d;
     }
     public static final class styleable {
         private styleable() {}
 
-        public static final int[] CircleImageView = { 0x7f030049, 0x7f03004a, 0x7f03004b, 0x7f03004c };
+        public static final int[] CircleImageView = { 0x7f03004a, 0x7f03004b, 0x7f03004c, 0x7f03004d };
         public static final int CircleImageView_civ_border_color = 0;
         public static final int CircleImageView_civ_border_overlay = 1;
         public static final int CircleImageView_civ_border_width = 2;
