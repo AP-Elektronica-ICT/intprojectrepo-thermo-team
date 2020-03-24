504c67ddb368dcadc72d9a29bdcea97f34a61c4f
Dries <s110169@ap.be>
2020-03-24 21:27:14 +0100

    Rare commit 2

--- thermo-sauna/app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/constraintlayout/widget/R.java
+++ thermo-sauna/app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/constraintlayout/widget/R.java
@@ -73,20 +73,20 @@ public final class R {
     public static final class id {
         private id() {}
 
-        public static final int bottom = 0x7f0a0058;
-        public static final int end = 0x7f0a0094;
-        public static final int gone = 0x7f0a00a7;
-        public static final int invisible = 0x7f0a00b5;
-        public static final int left = 0x7f0a00bb;
-        public static final int packed = 0x7f0a00ef;
-        public static final int parent = 0x7f0a00f2;
-        public static final int percent = 0x7f0a00f7;
-        public static final int right = 0x7f0a00ff;
-        public static final int spread = 0x7f0a0128;
-        public static final int spread_inside = 0x7f0a0129;
-        public static final int start = 0x7f0a012e;
-        public static final int top = 0x7f0a014d;
-        public static final int wrap = 0x7f0a0160;
+        public static final int bottom = 0x7f0a005b;
+        public static final int end = 0x7f0a0097;
+        public static final int gone = 0x7f0a00aa;
+        public static final int invisible = 0x7f0a00b8;
+        public static final int left = 0x7f0a00be;
+        public static final int packed = 0x7f0a00f2;
+        public static final int parent = 0x7f0a00f5;
+        public static final int percent = 0x7f0a00fa;
+        public static final int right = 0x7f0a0102;
+        public static final int spread = 0x7f0a012b;
+        public static final int spread_inside = 0x7f0a012c;
+        public static final int start = 0x7f0a0131;
+        public static final int top = 0x7f0a0150;
+        public static final int wrap = 0x7f0a0163;
     }
     public static final class styleable {
         private styleable() {}
