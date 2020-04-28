7a06e0ea6f3f67f0625da08cb9f4b3a0d1800bd2
Philip De Rudder <philip_de_rudder@hotmail.com>
2020-04-28 12:33:40 +0200

    added code vincent

--- thermo-sauna/app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/constraintlayout/widget/R.java
+++ thermo-sauna/app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/constraintlayout/widget/R.java
@@ -82,11 +82,11 @@ public final class R {
         public static final int parent = 0x7f0a00e7;
         public static final int percent = 0x7f0a00ec;
         public static final int right = 0x7f0a00f7;
-        public static final int spread = 0x7f0a011f;
-        public static final int spread_inside = 0x7f0a0120;
-        public static final int start = 0x7f0a0125;
-        public static final int top = 0x7f0a0143;
-        public static final int wrap = 0x7f0a0158;
+        public static final int spread = 0x7f0a0120;
+        public static final int spread_inside = 0x7f0a0121;
+        public static final int start = 0x7f0a0126;
+        public static final int top = 0x7f0a0144;
+        public static final int wrap = 0x7f0a0159;
     }
     public static final class styleable {
         private styleable() {}
--- thermo-sauna/app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/coordinatorlayout/R.java
+++ thermo-sauna/app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/coordinatorlayout/R.java
@@ -147,20 +147,20 @@ public final class R {
         public static final int right = 0x7f0a00f7;
         public static final int right_icon = 0x7f0a00f8;
         public static final int right_side = 0x7f0a00f9;
-        public static final int start = 0x7f0a0125;
-        public static final int tag_accessibility_actions = 0x7f0a012a;
-        public static final int tag_accessibility_clickable_spans = 0x7f0a012b;
-        public static final int tag_accessibility_heading = 0x7f0a012c;
-        public static final int tag_accessibility_pane_title = 0x7f0a012d;
-        public static final int tag_screen_reader_focusable = 0x7f0a012e;
-        public static final int tag_transition_group = 0x7f0a012f;
-        public static final int tag_unhandled_key_event_manager = 0x7f0a0130;
-        public static final int tag_unhandled_key_listeners = 0x7f0a0131;
-        public static final int text = 0x7f0a0134;
-        public static final int text2 = 0x7f0a0135;
-        public static final int time = 0x7f0a013f;
-        public static final int title = 0x7f0a0140;
-        public static final int top = 0x7f0a0143;
+        public static final int start = 0x7f0a0126;
+        public static final int tag_accessibility_actions = 0x7f0a012b;
+        public static final int tag_accessibility_clickable_spans = 0x7f0a012c;
+        public static final int tag_accessibility_heading = 0x7f0a012d;
+        public static final int tag_accessibility_pane_title = 0x7f0a012e;
+        public static final int tag_screen_reader_focusable = 0x7f0a012f;
+        public static final int tag_transition_group = 0x7f0a0130;
+        public static final int tag_unhandled_key_event_manager = 0x7f0a0131;
+        public static final int tag_unhandled_key_listeners = 0x7f0a0132;
+        public static final int text = 0x7f0a0135;
+        public static final int text2 = 0x7f0a0136;
+        public static final int time = 0x7f0a0140;
+        public static final int title = 0x7f0a0141;
+        public static final int top = 0x7f0a0144;
     }
     public static final class integer {
         private integer() {}
